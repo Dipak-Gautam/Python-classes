ec1fe282914faa6ec8e5aec44af3eacb683a0c40
Dipak-Gautam <anjangautam095@gmail.com>
2025-06-09 12:26:35 +0545

    instruction : first class introduction completed

---
+++ FirstClass/FirstProgram.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a number")
+num2 = input("Enter a second number")
+
+
+sum = int(num1) + int(num2)
+
+
+print("the sum of number", num1, "&", "number", num2, "is =", sum)
---
+++ FirstClass/Introduction.py
@@ -0,0 +1,9 @@
+num1 = input("Enter a first number : ")
+num2 = input("Enter a second number : ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+sum = num1 - num2
+
+print("The difference of two number is", sum)
